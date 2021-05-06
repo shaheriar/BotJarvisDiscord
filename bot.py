d1d5f9213ef26bba90781c9ec9cc4728e1fc6c26
shaheriar <shaheriarm@gmail.com>
2021-05-05 18:07:32 -0700

    music implementation

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 # bot.py
 
 import os
+from youtube_dl import YoutubeDL
 import random
 from Parse import parseForTrans
+from discord.utils import get
+from requests import get
 import discord
+from discord.ext import commands,tasks
 import praw
 import requests
 import json
@@ -25,7 +29,6 @@ base_url = "http://api.openweathermap.org/data/2.5/weather?"
 base_news_url =  "https://newsapi.org/v2/top-headlines?language=en&q="
 newsapi = NewsApiClient(api_key=ss.newskey)
 
-client = discord.Client()
 
 reddit = praw.Reddit(client_id=ss.redditid,
                      client_secret=ss.redditsecret,
@@ -33,23 +36,116 @@ reddit = praw.Reddit(client_id=ss.redditid,
                      user_agent=ss.useragent,
                      username=ss.redditusername)
 
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!',intents=intents)
+bot.remove_command('help')
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+
 def jokes(f):
     
     data = requests.get(f)
     tt = json.loads(data.text)
     return tt
 
-@client.event
+@bot.event
 async def on_ready():
     game = discord.Game("!help")
-    await client.change_presence(activity=game)
-    for guild in client.guilds:
+    await bot.change_presence(activity=game)
+    for guild in bot.guilds:
         print(f'{client.user} is connected to the following guild:\n{guild.name}(id: {guild.id})\n')
         if guild.name == GUILD:
             break
 
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
+
+##################### M U S I C #######################
+
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+@bot.command(name='leave')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+def search(query):
+    with YoutubeDL({'format': 'bestaudio', 'noplaylist':'True'}) as ydl:
+        try: requests.get(query)
+        except: info = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]
+        else: info = ydl.extract_info(query, download=False)
+    return (info, info['formats'][0]['url'])
+
+@bot.command(name='play_song')
+async def play(ctx, *, query):
+    await join(ctx)
+    #Solves a problem I'll explain later
+    FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+    video, source = search(query)
+    server = ctx.message.guild
+    voice = server.voice_client
+    title = video['title']
+
+    #await join(ctx)
+    await ctx.send(f'Now playing ' + title)
+
+    voice.play(discord.FFmpegPCMAudio(source, **FFMPEG_OPTS), after=lambda e: print('done', e))
+    voice.is_playing()
+
+
+@bot.command(name='pause')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.pause()
+        await ctx.send("Paused.")
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+    
+@bot.command(name='resume')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        await voice_client.resume()
+        await ctx.send("Resuming.")
+    else:
+        await ctx.send("The bot was not playing anything before this. Use play_song command")
+
+@bot.command(name='stop')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.stop()
+        await ctx.send("Stopped.")
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
 
     #################### W I K I P E D I A ######################
 
@@ -150,7 +246,7 @@ async def on_message(message):
         except discord.errors.HTTPException:
             return
 
-        
+
     ###################### R E D D I T ########################
 
     if message.content.startswith('!reddit'):
@@ -402,25 +498,30 @@ async def on_message(message):
 
     #################### H E L P ######################
 
-    if message.content.startswith('!help'):
-        words = message.content
-        line1 = '**HELP PAGE\n'
-        line2 = '__LIST OF COMMANDS__**\n'
-        line3 = '**!help** : Opens the help page\n'
-        line4 = '**!define {word}** : Get a one sentence definition of anything\n'
-        line5 = '**!summary {word}** : Get a more in depth definition of anything\n'
-        line6 = '**!search {word}** : Search for keywords\n'
-        line7 = '**!weather {city}** : Get weather info for any city\n'
-        line8 = '**!t {source} {destination} {text}** : Translate anything from source language to destination language\n'
-        line9 = '**!langs** : Get a list of supported languages to translate\n'
-        line10 = '**!news {topic}** : Get a list of news you\'re searching for\n'
-        line11 = '**!coin** : Flip a coin!\n'
-        line12 = '**!8ball {text}** : Let the magic 8-Ball decide your fate\n'
-        line13 = '**!dice** : Roll a dice and get a random number from 1 to 6\n'
-        line14 = '**!reddit {subreddit}** : Get the top 5 posts in a subreddit\n'
-        line15 = '**!invite** : Invite me to other servers!\n'
-        helptext = line1+line2+line3+line4+line5+line6+line7+line8+line9+line10+line11+line12+line13+line14+line15
-        await message.channel.send(helptext)
+@bot.command(name='help')
+async def help(ctx):
+    line1 = '**HELP PAGE\n'
+    line2 = '__LIST OF COMMANDS__**\n'
+    line3 = '**!help** : Opens the help page\n'
+    line4 = '**!define {word}** : Get a one sentence definition of anything\n'
+    line5 = '**!summary {word}** : Get a more in depth definition of anything\n'
+    line6 = '**!search {word}** : Search for keywords\n'
+    line7 = '**!weather {city}** : Get weather info for any city\n'
+    line8 = '**!t {source} {destination} {text}** : Translate anything from source language to destination language\n'
+    line9 = '**!langs** : Get a list of supported languages to translate\n'
+    line10 = '**!news {topic}** : Get a list of news you\'re searching for\n'
+    line11 = '**!coin** : Flip a coin!\n'
+    line12 = '**!8ball {text}** : Let the magic 8-Ball decide your fate\n'
+    line13 = '**!dice** : Roll a dice and get a random number from 1 to 6\n'
+    line14 = '**!reddit {subreddit}** : Get the top 5 posts in a subreddit\n'
+    line15 = '**!invite** : Invite me to other servers!\n'
+    line16 = '**!play_song {query}** : Search or paste a url to play a song in your voice channel\n'
+    line17 = '**!leave** : Leave your voice channel\n'
+    line18 = '**!pause** : Pause the current song\n'
+    line19 = '**!stop** : Stop the current song\n'
+
+    helptext = line1+line2+line3+line4+line5+line6+line7+line8+line9+line10+line11+line12+line13+line14+line15+line16+line17+line18+line19
+    await ctx.send(helptext)
 
     #################### T R A N S L A T E ######################
 
@@ -473,4 +574,6 @@ async def pages(ctx):
             break
             # ending the loop if user doesn't react after x seconds
 
-client.run(TOKEN)
+
+
+bot.run(TOKEN)
